7ed5c394f6cf1d6d2921b8b076991d12a3b3293d
Hexagonale <przemyslaw@cdot.app>
2021-01-19 19:34:45 +0100

    Fixed default constructor for SingleLineChartStyle

--- lib/src/styles/chartStyle.dart
+++ lib/src/styles/chartStyle.dart
@@ -10,23 +10,13 @@ abstract class ChartStyle {
   final EdgeInsets plotPadding;
 
   const ChartStyle({
-    this.backgroundColor = const Color(0xffffffff),
-    this.horizontalLinesStyle = const ChartLineStyle(
-      color: const Color(0xfff4f5f9),
-    ),
-    this.verticalLinesStyle = const ChartLineStyle(
-      draw: false,
-    ),
-    this.horizontalPointsCount = 5,
-    this.verticalPointsCount = 5,
-    this.horizontalAxisTextStyle = const TextStyle(
-      color: const Color(0xff858b9f),
-      fontSize: 10,
-    ),
-    this.verticalAxisTextStyle = const TextStyle(
-      color: const Color(0xff858b9f),
-      fontSize: 10,
-    ),
-    this.plotPadding = const EdgeInsets.only(left: 10, bottom: 10),
+    @required this.backgroundColor,
+    @required this.horizontalLinesStyle,
+    @required this.verticalLinesStyle,
+    @required this.horizontalPointsCount,
+    @required this.verticalPointsCount,
+    @required this.horizontalAxisTextStyle,
+    @required this.verticalAxisTextStyle,
+    @required this.plotPadding,
   });
 }
